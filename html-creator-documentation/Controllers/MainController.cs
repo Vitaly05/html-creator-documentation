2b8d060dd2c6c1b07af4ed28774cc5e3591029e8
Vitaly05 <vitalik050405@gmail.com>
2023-05-11 17:09:29 +0300

    Added library downloading

--- html-creator-documentation/Controllers/MainController.cs
+++ html-creator-documentation/Controllers/MainController.cs
@@ -5,6 +5,10 @@ namespace html_creator_documentation.Controllers
     [ApiController]
     public class MainController : Controller
     {
-
+        [HttpGet("/download")]
+        public FileResult GetLibraryFile()
+        {
+            return File("Files/html-creator-library.dll", "application/octet-stream", "html-creator-library.dll");
+        }
     }
 }
