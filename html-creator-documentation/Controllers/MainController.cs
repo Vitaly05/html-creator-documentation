003aea11d34bf10af09aa7f774be4d95f1900f1e
Виталий Лозюк <vitalik050405@gmail.com>
2023-05-11 08:41:57 +0300

    Add project files.

---
+++ html-creator-documentation/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace html_creator_documentation.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
