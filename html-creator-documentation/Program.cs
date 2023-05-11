496baabeb7971b7da12a8a2c0831dfb54930d804
Vitaly05 <vitalik050405@gmail.com>
2023-05-11 11:02:05 +0300

    Fixed site launching

--- html-creator-documentation/Program.cs
+++ html-creator-documentation/Program.cs
@@ -4,8 +4,10 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 app.MapControllers();
 
-app.MapGet("/", async (context) => await context.Response.SendFileAsync("Root/index.html"));
+app.MapGet("/", async (context) => await context.Response.SendFileAsync("wwwroot/index.html"));
 
 app.Run();
